d2fac90ed84d2677461a2974d5545fb584388a58
derek-cassady <derek@cassadyweb.com>
2025-01-12 14:54:22 -0500

    Problem 1 and render sections

--- js/assignment.js
+++ js/assignment.js
@@ -6,3 +6,25 @@ const checkIfEvenElement = document.getElementById("check-if-even");
 const lostNumbersElement = document.getElementById("lost-numbers");
 
 const lostNumbers = [4, 8, 15, 16, 23, 42];
+
+//assign var
+let num1 = 9
+let num2 = 4
+
+// Function to calculate the remainder of two numbers
+function returnTheRemainder(num1, num2) {
+    return num1 % num2; 
+    // Returns remainder of num1 divided by num2
+}
+
+
+// Function to render
+function render() {
+    // Calculate and display the remainder
+    remainderElement.textContent = returnTheRemainder(num1, num2);
+}
+
+
+submissionBtn.addEventListener("click", function () {
+    render();
+});
